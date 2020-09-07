e4f2fe8bb637476b8075e246b10b4f9bcf9128d1
diana.silva <diana.silva@rebase.com.br>
2020-09-07 16:35:47 -0300

    Conditional render componentes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import Button from './Button';
 
-function App() {
+const buttonHistoryClient = <button>Histórico de cliente</button>;
+const buttonAddClient = <button>Cadastrar cliente</button>;
+const hasCustomer = true;
+
+const App = () => {
+
+  const renderShowHistory = () =>
+    <div>Historico de clientes, clique abaixo:
+      {buttonHistoryClient}
+    </div>;
+
+  const renderAddCustomer = () =>
+    <div>Clique abaixo para cadastrar um cliente:
+      {buttonAddClient}
+    </div>;
+
+  const customer = 'Diana Regina';
+
+  const showCustomer = () => hasCustomer ? customer : 'Sem cliente';
+
   return (
     <div className="App">
-      ola diana
-      <Button />
+      {showCustomer()}
+      {hasCustomer ? renderShowHistory() : renderAddCustomer() }
     </div>
   );
-}
+};
 
 export default App;
